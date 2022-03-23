6e192cb9b1640443dfd8688f208956eabf885d30
KenasiaTurner <kenasi123@gmail.com>
2022-03-22 22:52:21 -0400

    adds hit button function

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   const deal_button = document.getElementById('deal-button');
-  
+  const dealer = document.getElementById("dealer-hand");
+  const player = document.getElementById("player-hand");
+  const hit_button = document.getElementById('hit-button'); 
+
 
     deal_button.addEventListener("click", function(doSomething) {
       let dealerOrPlayer = "dealer";
       
-      const dealer = document.getElementById("dealer-hand");
-      const player = document.getElementById("player-hand");
+      //const dealer = document.getElementById("dealer-hand");
+      //const player = document.getElementById("player-hand");
       
-    
-
+  
         for (let index = 0;index < 4 ; index++) {
           let card = document.createElement("img");
           card.src = "images/2_of_clubs.png"; 
@@ -28,7 +30,29 @@ document.addEventListener('DOMContentLoaded', function() {
            dealerOrPlayer = "player";
         }
         }
-      }) 
+    }) 
+ 
+    // adds function to the hit button
+    hit_button.addEventListener("click", function() {
+    let dealerOrPlayer = "dealer";
+
+      for (let index = 0; index < 2;index++) {
+        let card = document.createElement("img");
+        card.src = "images/2_of_clubs.png"; 
+
+        if (index == 0 ) {
+          dealer.appendChild(card); 
+      } else { 
+          player.appendChild(card); 
+      } 
+
+      }
+
+
+
     })
+
+
+})
   
     
