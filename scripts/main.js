bcda5a1bb30bd6b1e7686604a9d4442f5bd96785
KenasiaTurner <kenasi123@gmail.com>
2022-03-22 22:16:43 -0400

    adds the deal button function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,34 @@
-window.addEventListener('DOMContentLoaded', function() {
-  // Execute after page load
-})
+document.addEventListener('DOMContentLoaded', function() { 
+  console.log("Test")
+
+
+  const deal_button = document.getElementById('deal-button');
+  
+
+    deal_button.addEventListener("click", function(doSomething) {
+      let dealerOrPlayer = "dealer";
+      
+      const dealer = document.getElementById("dealer-hand");
+      const player = document.getElementById("player-hand");
+      
+    
+
+        for (let index = 0;index < 4 ; index++) {
+          let card = document.createElement("img");
+          card.src = "images/2_of_clubs.png"; 
+        
+
+        if (dealerOrPlayer == "dealer" ) {
+            dealer.appendChild(card); 
+        } else { 
+            player.appendChild(card); 
+        } 
+
+        if(index == 1) {
+           dealerOrPlayer = "player";
+        }
+        }
+      }) 
+    })
+  
+    
